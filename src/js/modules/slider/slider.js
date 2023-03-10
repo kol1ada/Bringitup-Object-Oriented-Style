348734aeaa662142a9e4cb025849cd10369ca874
Koliada Oleksii <koliada.oleksii@gmail.com>
2023-03-10 10:36:24 +0100

    +fixBugWithSliders

--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -12,7 +12,11 @@ export default class Slider {
         this.btns = document.querySelectorAll(btns);
         this.prev = document.querySelector(prev)
         this.next = document.querySelector(next)
-        this.slides = this.container.children;
+        try {
+            this.slides = this.container.children;
+        } catch (e) {
+
+        }
         this.activeClass = activeClass;
         this.animate = animate;
         this.autoplay = autoplay;
